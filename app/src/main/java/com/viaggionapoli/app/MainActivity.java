45c89eb5d8b843626d0495bf0dbc1f714683c99a
micheleascione4 <michele.ascione4@gmail.com>
2026-09-21 02:25:53 +0200

    Add Android build file: app/src/main/java/com/viaggionapoli/app/MainActivity.java

---
+++ app/src/main/java/com/viaggionapoli/app/MainActivity.java
@@ -0,0 +1,32 @@
+package com.viaggionapoli.app;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        setContentView(webView);
+
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.getSettings().setAllowContentAccess(true);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return false;
+            }
+        });
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+}
